4f40507350c5faaded7cf978ecc9854125688de9
vincentyin101 <vayin@stu.naperville203.org>
2022-08-23 15:08:50 -0500

    bluej notes
    some notes

---
+++ My First BlueJ Project/Ch1Notes.java
@@ -0,0 +1,38 @@
+
+/**
+ * Vincent Yin
+ * 8/23/22
+ * Chapter 1 Notes
+ * Class notes on basic concepts
+ * Who helped me: 
+ */
+public class Ch1Notes
+{
+    
+
+    /**
+     * this our code notes
+     */
+    public static void main(String[] args)
+    {
+        //basic data types
+        double number = 8.9,number2;
+        int x = 9,y,z;
+        boolean choice = false;
+        char letter = 'b';
+        float number3 = 8;
+        long n2 = 3;
+        short n3 = 7;
+        
+        
+        //These are Classes and Objects
+        //Class     ID        memory      Constructor
+        //Name
+        Integer     n1 =      new         Integer(47);
+        String      word =    new         String("Vincent Yin");
+        Double      number4 = new         Double(6.7);
+        
+        System.out.print(word.length());
+        
+    }
+}
